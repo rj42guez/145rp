e605944472829e21875a419cb1292bcc0677254a
Raphael Justin C. Portuguez <62928512+rj42guez@users.noreply.github.com>
2022-08-25 17:50:13 +0800

    Update client.py

--- client.py
+++ client.py
@@ -7,17 +7,17 @@ def encodeMessage(m):
     return M
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-a', type=str, default='10.0.5.69')
-parser.add_argument('-s', type=int, default=9000)
-parser.add_argument('-c', type=int, default=6734)
-parser.add_argument('-i', type=str, default='6d93e931')
+parser.add_argument('-a', '--address', type=str, default='10.0.5.69')
+parser.add_argument('-s', '--server', type=int, default=9000)
+parser.add_argument('-c', '--client', type=int, default=6734)
+parser.add_argument('-i', '--identifier', type=str, default='6d93e931')
 
 args = parser.parse_args()
 
-iD = args.i
-portS = int(args.c)
-portR = int(args.s)
-ipR = args.a
+iD = args.identifier
+portS = int(args.client)
+portR = int(args.sserver)
+ipR = args.address
 
 student_id = iD
 transaction_id = '99999999'
